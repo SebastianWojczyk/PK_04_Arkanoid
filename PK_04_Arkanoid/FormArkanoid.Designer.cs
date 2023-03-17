ed34a79b27df39862c591d4faa8499c9f87bab5b
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2023-03-17 12:02:19 +0100

    Piłeczka odbija się i spada (przegrana)

---
+++ PK_04_Arkanoid/FormArkanoid.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace PK_04_Arkanoid
+{
+    partial class FormArkanoid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBoxBoard = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 40;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBoxBoard.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxBoard.Name = "pictureBox1";
+            this.pictureBoxBoard.Size = new System.Drawing.Size(800, 450);
+            this.pictureBoxBoard.TabIndex = 0;
+            this.pictureBoxBoard.TabStop = false;
+            // 
+            // FormArkanoid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxBoard);
+            this.Name = "FormArkanoid";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FormArkanoid_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox pictureBoxBoard;
+    }
+}
+
---
+++ PK_04_Arkanoid/FormArkanoid.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PK_04_Arkanoid
+{
+    public partial class FormArkanoid : Form
+    {
+        Graphics g;
+        Rectangle Ball;
+        Point Direction;
+        public FormArkanoid()
+        {
+            InitializeComponent();
+        }
+
+        private void FormArkanoid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+            }
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            
+
+            if (Ball.X < 0 || Ball.X + Ball.Width > pictureBoxBoard.Width)
+            {
+                Direction.X = -Direction.X;
+            }
+            if (Ball.Y < 0)
+            {
+                Direction.Y = -Direction.Y;
+            }
+
+            Ball.X += Direction.X;
+            Ball.Y += Direction.Y;
+
+            g.Clear(Color.Black);
+            
+            g.FillEllipse(new SolidBrush(Color.Yellow), Ball);
+            g.DrawEllipse(new Pen(Color.Red, 5), Ball);
+            
+            pictureBoxBoard.Refresh();
+
+            if(Ball.Y + Ball.Height > pictureBoxBoard.Height)
+            {
+                timer.Stop();
+                MessageBox.Show("PRZEGRANA");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.pictureBoxBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+
+            pictureBoxBoard.Image = new Bitmap(pictureBoxBoard.Width, pictureBoxBoard.Height);
+            g = Graphics.FromImage(pictureBoxBoard.Image);
+
+            Ball = new Rectangle(300, 300, 100, 100);
+            Direction = new Point(15, 20);
+        }
+    }
+}
